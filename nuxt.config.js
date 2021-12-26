abf034b3d073e5e3d1981a30b57fbed12d3d85c6
Rei-Suzuki1729 <eva2021end@gmail.com>
2021-12-26 13:08:47 +0900

    CSS bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,8 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css', rel: 'stylesheet', integrity: 'sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC', crossorigin:'anonymous'}],
+
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
